ed36d6eb077f3bc20a66725a7f773d05e5f8f5bb
SDanDyS <gameking23@hotmail.com>
2020-09-22 06:07:29 -0700

    added Dir class and File class. File class not finished

---
+++ MemoryGame/Dir.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+/*
+ * The class is redundant for the memory game, but can be expanded for further use
+ */
+public class Dir
+{
+    public DirectoryInfo dirInfo { get; private set; }
+    private string path;
+
+    public Dir(string path)
+    {
+        this.path = path;
+        this.dirInfo = new DirectoryInfo(path);
+    }
+
+    public void Create()
+    {
+        if (!this.DirExists())
+        {
+            this.dirInfo.Create();
+        }
+    }
+
+    //make this a method in the interface
+    //so file and directories can be checked for existance 
+    public bool DirExists()
+    {
+        if (this.dirInfo.Exists)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void Delete(bool forceDelete = false)
+    {
+        switch (forceDelete)
+        {
+            case false:
+                this.dirInfo.Delete();
+                break;
+
+            case true:
+                this.dirInfo.Delete(true);
+                break;
+        }
+    }
+}
